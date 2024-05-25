07f9e81401086e5b6ccce1c38ef1eee5a32823e2
Keyel <keyel@MacBook-Air-de-Keyel.local>
2024-05-24 22:38:04 -0600

    [ADD] Screen

---
+++ src/TaskManager/Infrestructura/Vistas/Screen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View, StyleSheet} from 'react-native'
+
+const Screen = () => {
+    return (
+        <View>
+            <Text> textInComponent </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+
+
+export default Screen;
