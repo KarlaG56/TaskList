321062557adaf88fc27383c5326c27ccb1293eeb
Abril1231 <abriln2830@gmail.com>
2024-05-25 13:46:43 -0600

    sin tasklist

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import Navigation from "./Navigation";
 import HomeScreen from './src/TaskManager/Infrestructura/Vistas/HomeScreen';
-import InputScreen from './src/TaskManager/Infrestructura/Vistas/InputScreen';
 
-const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Input" component={InputScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+export default function App(){
+  return(
+    <HomeScreen navigation={undefined}/>
   );
 }
-
-export default App;
---
+++ Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/TaskManager/Infrestructura/Vistas/HomeScreen';
+import InputScreen from './src/TaskManager/Infrestructura/Vistas/InputScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Input" component={InputScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <App />
+        </NavigationContainer>
+    )
+}
